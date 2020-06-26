0d8cb75369294b783b39f20f2489d54cc1e3dd43
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-26 15:14:46 -0700

    restored functionality

--- _tests_/test.js
+++ _tests_/test.js
@@ -1,17 +1,27 @@
-import { UserInfo } from './../src/galaxy.js'
+import { UserInfo, ageCalc } from './../src/galaxy.js'
 
 // Begin test for ageCalc
 describe('ageCalc', () => {
-  test('Should fail if user age !== 41.66', () => {
-    let newUser = new UserInfo(29, 1, 75);
-    let userAge = newUser.ageCalc()
-    expect(userAge.toFixed(2)).toEqual("46.77");
+  test('Should fail if user age !== 120.83', () => {
+    let newUser = new UserInfo(29, 1, 10);
+    let userValues = Object.values(newUser);
+    expect(ageCalc(userValues[0], userValues[1], userValues[2]).toFixed(2)).toEqual("120.83");
   });
 });
 
 // Begin test for UserInfo
 describe('UserInfo', () => {
   test('Should fail if ths.age !== 29', () => {
+    let newUser = new UserInfo(29, 1, 75);
+    expect(newUser.age).toEqual(29);
+    expect(newUser.planetChoice).toEqual(1);
+    expect(newUser.lifeExp).toEqual(75);
+  });
+});
+
+// Begin test for Output
+describe('outPut', () => {
+  test('Should fail if it doesn not output an array', () => {
     let newUser = new UserInfo(29, 1, 75);
     expect(newUser.age).toEqual(29);
     expect(newUser.planetChoice).toEqual(1);
--- src/galaxy.js
+++ src/galaxy.js
@@ -4,73 +4,72 @@ export class UserInfo {
     this.age = age;
     this.planetChoice =  planetChoice;
     this.lifeExp = lifeExp;
-    console.log(this.age);
   }
-// Begin ageCal prototype function
-  ageCalc() {
-    let mercAge = this.age / .24;
-    let venusAge = this.age / .62;
-    let marAge = this.age / 1.88;
-    let jupAge = this.age / 11.86;
-  //Begin if statement to choose planet
-
-  //Begin Mercury
-    if (this.planetChoice === 1){
-      let lifeRemainder = (this.lifeExp - this.age) / .24;
-      if(this.age > this.lifeExp){
-        let exceedBy = (this.age - this.lifeExp) / .24;
-        console.log(`You would be ${ mercAge.toFixed(2) } years old on Mercury.`)
-        console.log(`Congrats! You've lived ${ exceedBy.toFixed(2) } Mercurial Years longer than expected!`)
-        return mercAge;
-      } else if ( this.age < this.lifExp) {
-        console.log(`You would be ${ mercAge.toFixed(2) } years old on Mercury.
-        You would have ${ lifeRemainder.toFixed(2) } Mercurial years left to live`);
-        return mercAge;
-      }
-    }
-  //Begin Venus
-      else if (planetChoice === 2){
-        let lifeRemainder = (lifeExp - age) / .24;
-        if(age > lifeExp){
-          let exceedBy = (age - lifeExp) / .24;
-          console.log(`You would be ${ venusAge.toFixed(2) } years old on Venus.`)
-          console.log(`Congrats! You've lived ${ exceedBy.toFixed(2) } Venetian Years longer than expected!`)
-          return venusAge;
-        } else if ( age < lifExp) {
-          console.log(`You would be ${ venusAge.toFixed(2) } years old on venus.
-          You would have ${ lifeRemainder.toFixed(2) } Venetian years left to live`);
-          return venetianAge;
-        }
-      }
-  //Begin Mars
-      else if (planetChoice === 3){
-        let lifeRemainder = (lifeExp - age) / .24;
-        if(age > lifeExp){
-          let exceedBy = (age - lifeExp) / .24;
-          console.log(`You would be ${ marAge.toFixed(2) } years old on Mars.`)
-          console.log(`Congrats! You've lived ${ exceedBy.toFixed(2) } Martian Years longer than expected!`)
-          return marAge;
-        } else if ( age < lifExp) {
-          console.log(`You would be ${ marsAge.toFixed(2) } years old on Mars.
-          You would have ${ lifeRemainder.toFixed(2) } Martian years left to live`);
-          return marsAge;
-        }
-      }
-  //Begin Jupiter
-      else if (planetChoice === 4){
-        let lifeRemainder = (lifeExp - age) / .24;
-        if(age > lifeExp){
-          let exceedBy = (age - lifeExp) / .24;
-          console.log(`You would be ${ jupAge.toFixed(2) } years old on Jupiter.`)
-          console.log(`Congrats! You've lived ${ exceedBy.toFixed(2) } Jupiter Years longer than expected!`)
-          return jupAge;
-        } else if ( age < lifExp) {
-          console.log(`You would be ${ jupAge.toFixed(2) } years old on Jupiter.
-          You would have ${ lifeRemainder.toFixed(2) } Jupital years left to live`);
-          return marsAge;
-        }
-      }
-    }
+  
 }
 
+// Begin Behavior 1
+export function  ageCalc(input, planetChoice, lifeExp) {
+  let mercAge = input / .24;
+  let venusAge = input / .62;
+  let marAge = input / 1.88;
+  let jupAge = input / 11.86;
+//Begin if statement to choose planet
 
+//Begin Mercury
+  if (planetChoice === 1){
+    let lifeRemainder = (lifeExp - input) / .24;
+    if(input > lifeExp){
+      let exceedBy = (input - lifeExp) / .24;
+      console.log(`You would be ${ mercAge.toFixed(2) } years old on Mercury.`)
+      console.log(`Congrats! You've lived ${ exceedBy.toFixed(2) } Mercurial Years longer than expected!`)
+      return mercAge;
+    } else if ( input < lifExp) {
+      console.log(`You would be ${ mercAge.toFixed(2) } years old on Mercury.
+      You would have ${ lifeRemainder.toFixed(2) } Mercurial years left to live`);
+      return mercAge;
+    }
+  }
+//Begin Venus
+    else if (planetChoice === 2){
+      let lifeRemainder = (lifeExp - input) / .24;
+      if(input > lifeExp){
+        let exceedBy = (input - lifeExp) / .24;
+        console.log(`You would be ${ venusAge.toFixed(2) } years old on Venus.`)
+        console.log(`Congrats! You've lived ${ exceedBy.toFixed(2) } Venetian Years longer than expected!`)
+        return venusAge;
+      } else if ( input < lifExp) {
+        console.log(`You would be ${ venusAge.toFixed(2) } years old on venus.
+        You would have ${ lifeRemainder.toFixed(2) } Venetian years left to live`);
+        return venetianAge;
+      }
+    }
+//Begin Mars
+    else if (planetChoice === 3){
+      let lifeRemainder = (lifeExp - input) / .24;
+      if(input > lifeExp){
+        let exceedBy = (input - lifeExp) / .24;
+        console.log(`You would be ${ marAge.toFixed(2) } years old on Mars.`)
+        console.log(`Congrats! You've lived ${ exceedBy.toFixed(2) } Martian Years longer than expected!`)
+        return marAge;
+      } else if ( input < lifExp) {
+        console.log(`You would be ${ marsAge.toFixed(2) } years old on Mars.
+        You would have ${ lifeRemainder.toFixed(2) } Martian years left to live`);
+        return marsAge;
+      }
+    }
+//Begin Jupiter
+    else if (planetChoice === 4){
+      let lifeRemainder = (lifeExp - input) / .24;
+      if(input > lifeExp){
+        let exceedBy = (input - lifeExp) / .24;
+        console.log(`You would be ${ jupAge.toFixed(2) } years old on Jupiter.`)
+        console.log(`Congrats! You've lived ${ exceedBy.toFixed(2) } Jupiter Years longer than expected!`)
+        return jupAge;
+      } else if ( input < lifExp) {
+        console.log(`You would be ${ jupAge.toFixed(2) } years old on Jupiter.
+        You would have ${ lifeRemainder.toFixed(2) } Jupital years left to live`);
+        return marsAge;
+      }
+    }
+  }
