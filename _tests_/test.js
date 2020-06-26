a129112d8e570af9231252361005efd9072300b5
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-26 10:08:43 -0700

    add all plents and logs for user age on those planets. Update to a more relevant age

--- _tests_/test.js
+++ _tests_/test.js
@@ -2,7 +2,7 @@ import { ageCalc } from './../src/galaxy.js'
 
 describe('ageCalc', () => {
   test('Should fail if user age !== 41.666', () => {
-    let userAge = 10;
-    expect(ageCalc(userAge).toFixed(2)).toEqual("41.67");
+    let userAge = 29;
+    expect(ageCalc(userAge).toFixed(2)).toEqual("120.83");
   });
 });
--- src/galaxy.js
+++ src/galaxy.js
@@ -1,13 +1,12 @@
 // Begin Behavior 1
 export function  ageCalc(input) {
-  let userAge = 10;
-  let planet = 1;
-  if (planet = 1) {
-  let mercAge = userAge / .24;
-  return mercAge;
-  }
-  // let venusAge = userAge / .62;
-  // let marAge = userAge / 1.88;
-  // let jupAge = userAge / 11.86;
+  let mercAge = input/ .24;
+  let venusAge = input / .62;
+  let marAge = input / 1.88;
+  let jupAge = input / 11.86;
+  console.log("You would be" + " " + (mercAge).toFixed(2) + " " + "years old on Mercury.");
+  console.log("You would be" + " " + (venusAge).toFixed(2) + " " + "years old on Venus.");
+  console.log("You would be" + " " + (marAge).toFixed(2) + " " + "years old on Mars.");
+  console.log("You would be" + " " + (jupAge).toFixed(2) + " " + "years old on Jupiter.");
   return mercAge;
   }
