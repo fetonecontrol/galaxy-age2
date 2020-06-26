4c2135aadd1e62f2378eb33d15daab525d0b684e
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-26 16:25:53 -0700

    make a note of stable version

--- src/main.js
+++ src/main.js
@@ -8,4 +8,5 @@ import { UserInfo, ageCalc } from './../src/galaxy.js'
 let newUser = new UserInfo(29, 1, 10);
 let userValues = Object.values(newUser);
 let  output = ageCalc(userValues[0], userValues[1], userValues[2])
-console.log(output[0], output[1]);
+console.log(output[0]);
+console.log(output[1]);
