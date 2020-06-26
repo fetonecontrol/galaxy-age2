77df9620238487f7ad138317888c0c5f13b0b7cf
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-26 09:50:15 -0700

    establish branching that will code for planet selection

--- src/galaxy.js
+++ src/galaxy.js
@@ -1,6 +1,13 @@
 // Begin Behavior 1
 export function  ageCalc(input) {
-  let userAge = 10
-  let mercAge = userAge / .24
+  let userAge = 10;
+  let planet = 1;
+  if (planet = 1) {
+  let mercAge = userAge / .24;
+  return mercAge;
+  }
+  // let venusAge = userAge / .62;
+  // let marAge = userAge / 1.88;
+  // let jupAge = userAge / 11.86;
   return mercAge;
   }
