c63b5fb811a105dfc5056802b75561c68758e0dc
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-26 10:25:06 -0700

    impliment template literals

--- src/galaxy.js
+++ src/galaxy.js
@@ -12,9 +12,9 @@ export function  ageCalc(input) {
   let venusAge = input / .62;
   let marAge = input / 1.88;
   let jupAge = input / 11.86;
-  console.log("You would be" + " " + (mercAge).toFixed(2) + " " + "years old on Mercury.");
-  console.log("You would be" + " " + (venusAge).toFixed(2) + " " + "years old on Venus.");
-  console.log("You would be" + " " + (marAge).toFixed(2) + " " + "years old on Mars.");
-  console.log("You would be" + " " + (jupAge).toFixed(2) + " " + "years old on Jupiter.");
+  console.log(`You would be ${ mercAge.toFixed(2) } years old on Mercury.`);
+  console.log("You would be" + " " + venusAge.toFixed(2) + " " + "years old on Venus.");
+  console.log("You would be" + " " + marAge.toFixed(2) + " " + "years old on Mars.");
+  console.log("You would be" + " " + jupAge.toFixed(2) + " " + "years old on Jupiter.");
   return mercAge;
   }
--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import './galaxy.js';
+import { UserInfo, ageCalc } from './../src/galaxy.js'
 // import $ from 'jquery';
