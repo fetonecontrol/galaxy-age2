b486765abbb38ad5c938f2522346b275de39db71
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-30 11:54:45 -0700

    translate function to prototype method for UserInfo class, add meaningful fail that validates the array return

--- _tests_/test.js
+++ _tests_/test.js
@@ -1,13 +1,13 @@
 import { UserInfo, ageCalc } from './../src/galaxy.js'
 
-// Begin test for ageCalc
-// describe('ageCalc', () => {
-//   test('Should fail if user age !== 120.83', () => {
-//     let newUser = new UserInfo(29, 1, 10);
-//     let userValues = Object.values(newUser);
-//     expect(ageCalc(userValues[0], userValues[1], userValues[2]).toFixed(2)).toEqual("120.83");
-//   });
-// });
+//Begin test for ageCalc
+describe('ageCalc', () => {
+  test('Should fail if user age !== 120.83', () => {
+    let newUser = new UserInfo(29, 1, 10);
+    let return1 = newUser.ageCalc();
+    expect(return1).toEqual(true);
+  });
+});
 
 // Begin test for UserInfo
 describe('UserInfo', () => {
@@ -20,11 +20,11 @@ describe('UserInfo', () => {
 });
 
 // Begin test for Output
-describe('outPut', () => {
-  test('Should fail if it doesn not output an array', () => {
-    let newUser = new UserInfo(29, 1, 75);
-    let userValues = Object.values(newUser);
-    let message = ageCalc(userValues[0], userValues[1], userValues[2])
-    console.log(message[0], message[1]);
-  });
-});
+// describe('outPut', () => {
+//   test('Should fail if it doesn not output an array', () => {
+//     let newUser = new UserInfo(29, 1, 75);
+//     let userValues = Object.values(newUser);
+//     let message = ageCalc(userValues[0], userValues[1], userValues[2])
+//     console.log(message[0], message[1]);
+//   });
+// });
--- src/galaxy.js
+++ src/galaxy.js
@@ -5,74 +5,73 @@ export class UserInfo {
     this.planetChoice =  planetChoice;
     this.lifeExp = lifeExp;
   }
-};
-
-// Begin Behavior 1
-export function  ageCalc(input, planetChoice, lifeExp) {
-  let mercAge = input / .24;
-  let venusAge = input / .62;
-  let marAge = input / 1.88;
-  let jupAge = input / 11.86;
+  // Begin Behavior 1
+  ageCalc() {
+  let mercAge = this.age / .24;
+  let venusAge = this.age / .62;
+  let marAge = this.age / 1.88;
+  let jupAge = this.age / 11.86;
   //Begin if statement to choose planet
 
   //Begin Mercury
-  if (planetChoice === 1){
+  if (this.planetChoice === 1){
     let message = [];
-    let lifeRemainder = (lifeExp - input) / .24;
-    if(input > lifeExp){
-      let exceedBy = (input - lifeExp) / .24;
+    let lifeRemainder = (this.lifeExp - this.age) / .24;
+    if(this.age > this.lifeExp){
+      let exceedBy = (this.age - this.lifeExp) / .24;
       message.push(`You would be ${ mercAge.toFixed(2) } years old on Mercury.`);
       message.push(`Congrats! You've lived ${ exceedBy.toFixed(2) } Mercurial Years longer than expected!`);
       return message;
-    } else if ( input < lifeExp) {
+    } else if ( this.age < this.lifefeExp) {
       message.push(`You would be ${ mercAge.toFixed(2) } years old on Mercury.
       You would have ${ lifeRemainder.toFixed(2) } Mercurial years left to live`);
       return message;
     }
   }
   //Begin Venus
-  else if (planetChoice === 2){
+  else if (this.planetChoice === 2){
     let message = [];
-    let lifeRemainder = (lifeExp - input) / .62;
-    if(input > lifeExp){
-      let exceedBy = (input - lifeExp) / .62;
+    let lifeRemainder = (this.lifeExp - this.age) / .62;
+    if(this.age > this.lifeExp){
+      let exceedBy = (this.age - this.lifeExp) / .62;
       message.push(`You would be ${ venusAge.toFixed(2) } years old on Venus.`);
       message.push(`Congrats! You've lived ${ exceedBy.toFixed(2) } Venetian Years longer than expected!`);
       return venusAge;
-    } else if ( input < lifeExp) {
+    } else if ( this.age < this.lifeExp) {
       message.push(`You would be ${ venusAge.toFixed(2) } years old on venus.`);
       message.push(`You would have ${ lifeRemainder.toFixed(2) } Venetian years left to live`);
       return venusAge;
     }
   }
   //Begin Mars
-  else if (planetChoice === 3){
+  else if (this.planetChoice === 3){
     let message = [];
-    let lifeRemainder = (lifeExp - input) / 1.88;
-    if(input > lifeExp){
-      let exceedBy = (input - lifeExp) / 1.88;
+    let lifeRemainder = (this.lifeExp - this.age) / 1.88;
+    if(this.age > this.lifeExp){
+      let exceedBy = (this.age - this.lifeExp) / 1.88;
       message.push(`You would be ${ marAge.toFixed(2) } years old on Mars.`);
       message.push(`Congrats! You've lived ${ exceedBy.toFixed(2) } Martian Years longer than expected!`);
       return marAge;
-    } else if ( input < lifeExp) {
+    } else if ( this.age < this.lifeExp) {
       message.push(`You would be ${ marAge.toFixed(2) } years old on Mars.`);
       message.push(`You would have ${ lifeRemainder.toFixed(2) } Martian years left to live`);
       return marAge;
     }
   }
   //Begin Jupiter
-  else if (planetChoice === 4){
+  else if (this.planetChoice === 4){
     let message = [];
-    let lifeRemainder = (lifeExp - input) / 11.86;
-    if(input > lifeExp){
-      let exceedBy = (input - lifeExp) / 11.86;
+    let lifeRemainder = (this.lifeExp - this.age) / 11.86;
+    if(this.age > this.lifeExp){
+      let exceedBy = (this.age - this.lifeExp) / 11.86;
       message.push(`You would be ${ jupAge.toFixed(2) } years old on Jupiter.`);
       message.push(`Congrats! You've lived ${ exceedBy.toFixed(2) } Jupiter Years longer than expected!`);
       return jupAge;
-    } else if ( input < lifeExp) {
+    } else if ( this.age < this.lifeExp) {
       message.push(`You would be ${ jupAge.toFixed(2) } years old on Jupiter.`);
       message.push(`You would have ${ lifeRemainder.toFixed(2) } Jupital years left to live`);
       return jupAge;
     }
   }
+}
 }
